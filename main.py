d93d8cbea5d5983f9d12d3271dac37074f007ba5
Moritz R <73069661+moritzrfs@users.noreply.github.com>
2024-11-14 07:59:30 +0000

    Add query applications

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 from graphql_client import GraphQLClient
 from queries.admin_queries import get_admins_query
+from queries.event_queries import get_events_query, get_events_feed_query
 from config import ACCOUNT_ID, API_KEY, API_ENDPOINT
 from utils import print_pretty_json
 
@@ -9,7 +10,16 @@ def main():
 
     limit = 10
 
-    query, variables = get_admins_query(ACCOUNT_ID, limit)
+    account_ids=[ACCOUNT_ID]
+    filters = [
+        {
+        "fieldName": "pop_name",
+        "operator": "is",
+        "values": ["Beijing_DC2"]
+        }
+    ]
+
+    query, variables = get_events_feed_query(account_ids, filters)
 
     try:
         response = client.execute_query(query, variables)
